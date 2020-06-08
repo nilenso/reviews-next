2a4e3039b8c8d90bb7870f65c7f29d7f0fe4b372
Govind Krishna Joshi <govind@nilenso.com>
2020-06-09 03:20:21 +0530

    Add styling for the login page

--- src/cljs/reviews_next/core.cljs
+++ src/cljs/reviews_next/core.cljs
@@ -4,12 +4,9 @@
             [reviews-next.pages.login :as login]))
 
 (defn app []
-  [:div
-   [:div "Hello!"]
-   [login/login]])
+  [login/login])
 
 (defn init! []
-  (apply (.-log js/console) "Hello World!")
   (re-frame/dispatch-sync [:initialize-db])
   (re-frame/dispatch-sync [:setup-google-signin-functions])
   (rdom/render [app] (.getElementById js/document "app")))
--- src/cljs/reviews_next/pages/login.cljs
+++ src/cljs/reviews_next/pages/login.cljs
@@ -1,6 +1,17 @@
 (ns reviews-next.pages.login)
 
+(defn logo []
+  [:img {:src "assets/images/vertical.svg"}])
+
+(defn login-button []
+  [:div {:class ["g-signin2"]
+         :data-onsuccess "onSignIn"
+         :data-width "240"
+         :data-heigth "50"
+         :data-theme "dark"
+         :data-longtitle "240"}])
+
 (defn login []
-  [:div.container
-   [:div {:class "g-signin2"
-          :data-onsuccess "onSignIn"}]])
+  [:div {:class "login container" }
+   [logo]
+   [login-button]])
