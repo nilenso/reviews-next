50f6e7f735371ce81789ff5aad865394d7ec2e6a
Govind Krishna Joshi <govind@nilenso.com>
2021-03-20 14:18:56 +0530

    Add request logging

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,15 @@
 module Main where
 
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Middleware.RequestLogger (logStdout)
+
 import Reviews.Routes (app)
 import Reviews.Types.Config
 
 main :: IO ()
 main = do
   config <- readConfig "./config.dhall"
-  run 3000 (app config)
+  putStrLn $ "Starting reviews-next on port: " ++ show (port config)
+  run 3000 $ application config
+  where
+  application = logStdout . app
