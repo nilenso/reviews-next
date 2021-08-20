35b7999da3453f72b4fef084bf838e502872afe6
Govind Krishna Joshi <govind@nilenso.com>
2021-08-20 12:23:20 +0530

    Remove old code

--- src/Reviews/Types/Common.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE RankNTypes #-}
-
-module Reviews.Types.Common where
-
-import Reviews.Types.Config ( Config )
-import Control.Effect.Reader (Has, Reader)
-
-type Handler a = forall sig m. (Has (Reader Config) sig m) => m a
